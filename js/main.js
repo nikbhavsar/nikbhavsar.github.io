db0840d6a5d0d39741e6968c11eabe1c68099b0e
Nikhar <bhavsarnikhar21@gmail.com>
2018-11-15 15:10:05 -0800

    Added Testimonial
    Added testimonial section
    Added some animation in javascript

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(function () {
     }, 1000);
 
     $(".loader").fadeOut(3000, function () {
-     
+
         $(".maincontent").fadeIn(500);
     });
 });
@@ -18,15 +18,22 @@ $(function () {
 // Header Animation
 
 var whoAmI = ["Software Developer", "Web Developer", "Android Developer"];
-var color = ["#FFFFFF", "#F0DF9D", "#C6C5C3"];
+var color = ["#D1C4DF", "#F0DF9D", "#C6C5C3"];
+var color1 = ["#F0DF9D", "#C6C5C3", "#D1C4DF"];
+var color2 = ["#C6C5C3", "#D1C4DF", "#F0DF9D"];
 var jobType = document.querySelector(".developer-span");
+var myName = document.querySelector(".name-span");
+var slogan = document.querySelector(".slogan-span");
 
 var i = 0;
 
 setInterval(function () {
-    jobType
-        .innerHTML = whoAmI[i];
-    jobType.style.color = color[i++];
+
+    jobType.innerHTML = whoAmI[i];
+    jobType.style.color = color[i];
+
+    myName.style.color = color1[i];
+    slogan.style.color = color2[i++];
 
     if (i == whoAmI.length) i = 0;
 }, 1250);
