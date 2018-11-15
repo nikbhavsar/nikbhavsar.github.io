ef7cb59e5a975bfbdb0b371c12fbb2ec887c8b93
Nikhar <bhavsarnikhar21@gmail.com>
2018-11-15 10:44:48 -0800

    Animation Added
    Add little animation to change the text of html

--- js/main.js
+++ js/main.js
@@ -15,9 +15,23 @@ $(function () {
     });
 });
 
-// Changing Letters in Jumbortron
+// Header Animation
 
-var child = document.querySelector("slogan-span");
+var whoAmI = ["Software Developer", "Web Developer", "Android Developer"];
+var color = ["#FFFFFF", "#F0DF9D", "#C6C5C3"];
+var jobType = document.querySelector(".developer-span");
+
+var i = 0;
+
+setInterval(function () {
+    jobType
+        .innerHTML = whoAmI[i];
+    jobType.style.color = color[i++];
+
+    if (i == whoAmI.length) i = 0;
+}, 1250);
+
+
 
 
 
